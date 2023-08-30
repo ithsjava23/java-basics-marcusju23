668f9bd4d380be8cc1d69c2c62ef5462c50a7ae1
marcusju23 <143085217+marcusju23@users.noreply.github.com>
2023-08-30 11:37:55 +0200

    BACKUP_TEST1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,44 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
+    Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        String menu = """
+                Elpriser
+                ========
+                1. Inmatning
+                2. Min, Max och Medel
+                3. Sortera
+                4. Bästa Laddningstid (4h)
+                e. Avsluta
+                """;
+        System.out.println(menu);
+        System.out.println("Välj ett alternativ");
+        String menuOption = sc.nextLine();
+
+
+        switch (menuOption) {
+            case '1' -> {
+            }
+            // code block
+            case '2' -> {
+            }
+            case '3' -> {
+
+            }
+            case '4' -> {
+
+            }
+            case 'e' -> {
+
+            }
+            // code block
+            default -> {
+            }
+            // code block
+        }
     }
 }
