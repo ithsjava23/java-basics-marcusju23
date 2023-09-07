19f0c423ab5e6c5b87343658090adc08b693969b
marcusju23 <143085217+marcusju23@users.noreply.github.com>
2023-09-07 13:41:39 +0200

    Update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,44 +3,61 @@ package org.example;
 import java.util.Scanner;
 
 public class App {
-    Scanner sc = new Scanner(System.in);
+    public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String menu = """
-                Elpriser
-                ========
-                1. Inmatning
-                2. Min, Max och Medel
-                3. Sortera
-                4. Bästa Laddningstid (4h)
-                e. Avsluta
-                """;
-        System.out.println(menu);
-        System.out.println("Välj ett alternativ");
-        String menuOption = sc.nextLine();
+        while (true) {
+            showMenu();
+            System.out.print("\nVälj ett alternativ ");
+            String menuOption = sc.nextLine().toLowerCase();
 
-//        test
-
-
-        switch (menuOption) {
-            case '1' -> {
+            switch (menuOption) {
+                case "1" -> inputData();
+                case "2" -> minMaxMid();
+                case "3" -> "".isEmpty();
+                case "4" -> "".isEmpty();
+                case "e" -> {
+                    System.out.print("Avslutar...\n");
+                    System.exit(0);
+                }
+                default -> System.out.print(menuOption + " är inget alternativ.\n");
             }
-            // code block
-            case '2' -> {
-            }
-            case '3' -> {
-
-            }
-            case '4' -> {
-
-            }
-            case 'e' -> {
-
-            }
-            // code block
-            default -> {
-            }
-            // code block
         }
+    }
+
+    public static void showMenu() {
+        System.out.print("""
+                    Elpriser
+                    ========
+                    1. Inmatning
+                    2. Min, Max och Medel
+                    3. Sortera
+                    4. Bästa Laddningstid (4h)
+                    e. Avsluta
+                    """);
+    }
+
+    public static void inputData() {
+        int[] electricityPrices = new int[24];
+
+        // eletricPricesMockdata = {60 65 70 68 72 75 80 85 90 88 82 78 76 75 72 70 68 65 60 58 55 50 48 45};
+
+        for (int hour = 0; hour < 24; hour++) {
+            System.out.print("Ange elpriset för timme " + String.format("%02d", hour) + ": ");
+            int price = Integer.parseInt(sc.nextLine());
+            // int price = sc.nextInt();
+        }
+    }
+
+    public static void printMinMaxMid(){
+        System.out.println("Lägsta pris: 02-03, 1 öre/kWh");
+        System.out.println("Högsta pris: 00-01, 100 öre/kWh");
+        System.out.println("Medelpris: 13,38 öre/kWh");
+    }
+
+    public static void printSortedData(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
     }
 }
---
+++ src/main/java/org/example/Presentation.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Presentation {
+        int min;
+        int max;
+        int mid;
+
+    public int getMin() {
+        return min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public int getMid() {
+        return mid;
+    }
+}
