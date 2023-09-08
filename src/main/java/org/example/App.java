492a8afbb03c136b6b287c91f4ac67d4441b1166
marcusju23 <143085217+marcusju23@users.noreply.github.com>
2023-09-08 15:48:58 +0200

    Update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,17 +4,20 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
-    public static Scanner sc = new Scanner(System.in);
+    public Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Locale.setDefault(Locale.forLanguageTag("sv-SE"));
+        Locale swedishLocale = new Locale("sv", "SE");
+        Locale.setDefault(swedishLocale);
+        Scanner sc = new Scanner(System.in);
+
         while (true) {
             showMenu();
             System.out.print("\nVälj ett alternativ ");
             String menuOption = sc.nextLine().toLowerCase();
 
             switch (menuOption) {
-                case "1" -> inputData();
+                case "1" -> inputData(sc);
                 case "2" -> "".isEmpty();
                 case "3" -> "".isEmpty();
                 case "4" -> "".isEmpty();
@@ -39,22 +42,43 @@ public class App {
                 """);
     }
 
-    public static void inputData() {
+    public static void inputData(Scanner sc) {
+        int[] hours = new int[24];
         int[] electricityPrices = new int[24];
 
-        // eletricPricesMockdata = {60 65 70 68 72 75 80 85 90 88 82 78 76 75 72 70 68 65 60 58 55 50 48 45};
+        // priceMockData = 60 65 70 68 72 75 80 85 90 88 82 78 76 75 72 70 68 65 60 58 55 50 48 45
 
         for (int hour = 0; hour < 24; hour++) {
             System.out.print("Ange elpriset för timme " + String.format("%02d", hour) + ": ");
-            int price = Integer.parseInt(sc.nextLine());
-            // int price = sc.nextInt();
+            int price = sc.nextInt();
+            electricityPrices[hour] = price;
         }
+        System.out.print("Elpriser: ");
+        for (int hour = 0; hour < 24; hour++) {
+            System.out.print(electricityPrices[hour] + " ");
+        }
+        sc.nextLine();
     }
 
-    public static void printMinMaxMid() {
-        System.out.println("Lägsta pris: 02-03, 1 öre/kWh");
-        System.out.println("Högsta pris: 00-01, 100 öre/kWh");
-        System.out.println("Medelpris: 13,38 öre/kWh");
+    public static void printMinMaxMid(int[] eletricityPrices) {
+        int minPrice = eletricityPrices[0];
+        int maxPrice = eletricityPrices[0];
+        double midPrice;
+        int sum = 0;
+
+        for (int price : eletricityPrices) {
+            if (price < minPrice) {
+                minPrice = price;
+            }
+            if (price > maxPrice) {
+                maxPrice = price;
+            }
+            sum += price;
+        }
+        midPrice = (double) sum/eletricityPrices.length;
+        System.out.println("Lägsta pris: " );
+        System.out.println();
+        System.out.println();
     }
 
     public static void printSortedData(int[] array) {
